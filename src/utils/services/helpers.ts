6150631e9a30818140f05aa5b45a3b882d67ad11
azam.chaudhary <azam.chaudhary@speridian.com>
2022-10-11 09:20:00 -0400

    latest pull taken

--- src/utils/services/helpers.ts
+++ src/utils/services/helpers.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 
-// export const baseUrl = `http://localhost:9999`;
-export const baseUrl = `https://b47a-59-94-73-204.in.ngrok.io`;
+export const baseUrl = `http://localhost:9999`;
+// export const baseUrl = `https://b47a-59-94-73-204.in.ngrok.io`;
 export const webUrl = `http://localhost:3000`
 const colors = [
   "#f4511e",
