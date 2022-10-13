37ab2ec617755b60689bdc5d10e9d4f39665aa5c
Diksha Deep <Diksha Deep@SPBLRLTU658.speridian.com>
2022-10-13 17:04:38 +0530

    change the folder

---
+++ src/query/search.ts
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+
+export const places = gql`
+query SearchTitle($search_one: String!, $search_two: String!){
+  places(filters: { or:[{title: {contains:$search_one}},{title: {contains:$search_two}}] }) {
+    data {
+      id
+      attributes{
+        title
+        description
+      }
+    }
+  }
+}
+`;
+
+export const events = gql`
+query SearchTitle($search_one: String!, $search_two: String!){
+  events(filters: { or:[{title: {contains:$search_one}},{title: {contains:$search_two}}] }) {
+    data {
+      id
+      attributes{
+        title
+        description
+      }
+    }
+  }
+}
+`;
