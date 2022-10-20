aa4b1e1ccf144269bec85d6b73a5e104396d62dc
Diksha Deep <Diksha Deep@SPBLRLTU658.speridian.com>
2022-10-20 22:15:39 +0530

    add obj

--- src/hooks/usePlace.ts
+++ src/hooks/usePlace.ts
@@ -7,19 +7,18 @@ import { RootState } from "../store";
 import {
   setPlaces,
   setPlaceMetaData,
-  setSearchText
+  setSearchText,
 } from "../store/reducers/searchResultsReducer";
-import {limit} from '../utils/services/helpers';
+import { limit } from "../utils/services/helpers";
 
 const usePlace = () => {
   const [hasMoreData, setHasMoreData] = useState(true);
-  const {
-    searchText,
-    places: placeData,
-  } = useSelector((state: RootState) => state.searchResults);
-  const {
-    selectedValue
-  } = useSelector((state: RootState) => state.refinedSearch);
+  const { searchText, places: placeData } = useSelector(
+    (state: RootState) => state.searchResults
+  );
+  const { selectedValue } = useSelector(
+    (state: RootState) => state.refinedSearch
+  );
   const { search } = useLocation();
   const dispatch = useDispatch();
 
@@ -40,33 +39,72 @@ const usePlace = () => {
 
   useEffect(() => {
     if (data?.places) {
-      dispatch(setSearchText(decodeURIComponent(search.replace("?search=", ""))))
+      dispatch(
+        setSearchText(decodeURIComponent(search.replace("?search=", "")))
+      );
       // update the data for the pagination
-      if (data?.places.meta.pagination.page === 1 && data?.places.data.length > 0) {
+      if (
+        data?.places.meta.pagination.page === 1 &&
+        data?.places.data.length > 0
+      ) {
         dispatch(setPlaces([...data?.places.data]));
       } else if (data?.places.data.length > 0) {
         dispatch(setPlaces([...placeData, ...data?.places.data]));
       }
       // update the meta data
       dispatch(setPlaceMetaData(data?.places?.meta));
-      // this flag decides to fetch next set of data 
-      setHasMoreData(data?.places?.meta.pagination.pageCount !==
-        data?.places.meta.pagination.page);
-
+      // this flag decides to fetch next set of data
+      setHasMoreData(
+        data?.places?.meta.pagination.pageCount !==
+          data?.places.meta.pagination.page
+      );
     }
   }, [data]);
 
-  const fetchData = (skip: number = placeData.length, local: boolean = false) => {
-    const text = local ? searchText : decodeURIComponent(search.replace("?search=", ""));
+  const fetchData = (
+    skip: number = placeData.length,
+    local: boolean = false
+  ) => {
+    const text = local
+      ? searchText
+      : decodeURIComponent(search.replace("?search=", ""));
     const copiedValue = JSON.parse(JSON.stringify(selectedValue));
-    Object.keys(copiedValue).map(x => {
-      if (copiedValue[x].length === 0) {delete copiedValue[x];}
+    Object.keys(copiedValue).map((x) => {
+      if (copiedValue[x].length === 0) {
+        delete copiedValue[x];
+      }
       return x;
     });
+    console.log('copiedValue', copiedValue)
     const searchParams = new URLSearchParams(copiedValue);
-    console.log('decodeURIComponent', decodeURIComponent(searchParams.toString()))
-    const searchWordArray = text.split(' ');
-    refetchPlaces({ search_one: searchWordArray[0], search_two: searchWordArray[1], search_three: searchWordArray[2], limit: limit, skip: skip });
+    // console.log(
+    //   "decodeURIComponent",
+    //   decodeURIComponent(searchParams.toString())
+    // );
+    const searchWordArray = text.split(" ");
+    const obj: any = {
+      // researchValue: copiedValue.researchValue ? copiedValue?.researchValue[0] : "",
+      // tourismValue: copiedValue.tourismValue ? copiedValue?.tourismValue[0] : "",
+      // stateOfConservation: copiedValue.stateOfConservation ? copiedValue?.stateOfConservation[0] : "",
+      // recommendation: copiedValue?.recommendation > 0 ? copiedValue?.recommendation[0] : "",
+      // risk: copiedValue?.risk[0] || "",
+      // period: copiedValue?.period[0] || "",
+      // latitude: copiedValue?.latitude,
+      // longitude: copiedValue?.longitude,
+      // language: Object.keys(copiedValue).length > 0 ? "English" : "",
+      
+    };
+    if (Object.keys(copiedValue).length > 0) {
+      obj.language = "English";
+    }
+    console.log('obj', obj);
+    refetchPlaces({
+      ...obj,
+      search_one: searchWordArray[0],
+      search_two: searchWordArray[1],
+      search_three: searchWordArray[2],
+      limit: limit,
+      skip: skip,});
   };
 
   return {
--- src/query/places.ts
+++ src/query/places.ts
@@ -1,90 +1,172 @@
 import { gql } from "@apollo/client";
 
 export const places = gql`
-  query SearchTitle($search_one: String, $search_two: String, $search_three: String, $limit: Int, $skip: Int) {
-    places(
-      pagination: {
-        limit: $limit
-        start: $skip
-      }
-      filters: {
-        or: [
-          # {siteType: { containsi: $search_one }}
-          {siteDescription:{containsi:$search_one }}
-          # {period: { containsi: $search_one }}
-          {previousNumber:{containsi:$search_one }}
-          {placeNumber:{containsi:$search_one }}
-          {placeNameEnglish: { containsi: $search_one }}
-          {placeNameArabic:{containsi:$search_one}}
-          {keywords:{containsi:$search_one}}
-          
-                    # {siteType: { containsi: $search_two }}
-          {siteDescription:{containsi:$search_two }}
-          # {period: { containsi: $search_two }}
-          {previousNumber:{containsi:$search_two }}
-          {placeNumber:{containsi:$search_two }}
-          {placeNameEnglish: { containsi: $search_two }}
-          {placeNameArabic:{containsi:$search_two}}
-          {keywords:{containsi:$search_two}}
-
-          
-                    # {siteType: { containsi: $search_three }}
-          {siteDescription:{containsi:$search_three }}
-          # {period: { containsi: $search_three }}
-          {previousNumber:{containsi:$search_three }}
-          {placeNumber:{containsi:$search_three }}
-          {placeNameEnglish: { containsi: $search_three }}
-          {placeNameArabic:{containsi:$search_three}}
-          {keywords:{containsi:$search_three}}
-        ]
-      }
-    ) {
-      meta {
-        pagination {
-          total
-          pageCount
-          pageSize
-          page
+query SearchTitle(
+  $search_one: String
+  $search_two: String
+  $search_three: String
+  $researchValue: String
+  $tourismValue: String
+  $stateOfConservation: String
+  $recommendation: String
+  $risk: String
+  $period: String
+  $latitude: Float
+  $longitude: Float
+  $language: String
+  $limit: Int
+  $skip: Int
+) {
+  places(
+    pagination: { limit: $limit, start: $skip }
+    filters: {
+      and: [
+        {
+          latitude: {
+            containsi: $latitude
+          }
         }
+        {
+          longitude: {
+            containsi: $longitude 
+          }
+        },
+        {
+          period: {
+            translation: {
+              locale: {
+                or: [{ value: { containsi: $period } }]
+                languages: { name: { containsi: $language } }
+              }
+            }
+          }
+        }
+        {
+          risk: {
+            translation: {
+              locale: {
+                or: [{ value: { containsi: $risk } }]
+                languages: { name: { containsi: $language } }
+              }
+            }
+          }
+        }
+        {
+          recommendation: {
+            translation: {
+              locale: {
+                or: [{ value: { containsi: $recommendation } }]
+                languages: { name: { containsi: $language } }
+              }
+            }
+          }
+        }
+        {
+          researchValue: {
+            translation: {
+              locale: {
+                or: [{ value: { containsi: $researchValue } }]
+                languages: { name: { containsi: $language } }
+              }
+            }
+          }
+        }
+        {
+          stateOfConservation: {
+            translation: {
+              locale: {
+                or: [{ value: { containsi: $stateOfConservation } }]
+                languages: { name: { containsi: $language } }
+              }
+            }
+          }
+        }
+        {
+          tourismValue: {
+            translation: {
+              locale: {
+                or: [
+                  { value: { contains: $tourismValue } }
+                ]
+                languages: { name: { containsi: $language } }
+              }
+            }
+          }
+        }
+      ]
+      or: [
+        # {siteType: { containsi: $search_one }}
+        { siteDescription: { containsi: $search_one } }
+        # {period: { containsi: $search_one }}
+        { previousNumber: { containsi: $search_one } }
+        { placeNumber: { containsi: $search_one } }
+        { placeNameEnglish: { containsi: $search_one } }
+        { placeNameArabic: { containsi: $search_one } }
+        { keywords: { containsi: $search_one } }
+        # {siteType: { containsi: $search_two }}
+        { siteDescription: { containsi: $search_two } }
+        # {period: { containsi: $search_two }}
+        { previousNumber: { containsi: $search_two } }
+        { placeNumber: { containsi: $search_two } }
+        { placeNameEnglish: { containsi: $search_two } }
+        { placeNameArabic: { containsi: $search_two } }
+        { keywords: { containsi: $search_two } }
+        # {siteType: { containsi: $search_three }}
+        { siteDescription: { containsi: $search_three } }
+        # {period: { containsi: $search_three }}
+        { previousNumber: { containsi: $search_three } }
+        { placeNumber: { containsi: $search_three } }
+        { placeNameEnglish: { containsi: $search_three } }
+        { placeNameArabic: { containsi: $search_three } }
+        { keywords: { containsi: $search_three } }
+      ]
+    }
+  ) {
+    meta {
+      pagination {
+        total
+        pageCount
+        pageSize
+        page
       }
-      data {
-        id
-        attributes {
-          placeNameEnglish
-          placeNameArabic
-          type
-          siteDescription
-          updatedAt
-          keywords
-          placeNumber
-          latitude
-          longitude
-          uniqueId
-          period{
-            data{
-              id
-              attributes{
-                name
-              }
+    }
+    data {
+      id
+      attributes {
+        placeNameEnglish
+        placeNameArabic
+        type
+        siteDescription
+        updatedAt
+        keywords
+        placeNumber
+        latitude
+        longitude
+        uniqueId
+        period {
+          data {
+            id
+            attributes {
+              name
             }
           }
-          researchValue {
-            data {
-              id
-              attributes {
-                name
-                translation {
-                  data {
-                    id
-                    attributes {
-                      code
-                      locale {
-                        value
-                        languages {
-                          data {
-                            attributes {
-                              name
-                            }
+        }
+        researchValue {
+          data {
+            id
+            attributes {
+              name
+              translation {
+                data {
+                  id
+                  attributes {
+                    code
+                    locale {
+                      value
+                      languages {
+                        data {
+                          attributes {
+                            name
                           }
                         }
                       }
@@ -94,24 +176,24 @@ export const places = gql`
               }
             }
           }
+        }
 
-          tourismValue {
-            data {
-              id
-              attributes {
-                name
-                translation {
-                  data {
-                    id
-                    attributes {
-                      code
-                      locale {
-                        value
-                        languages {
-                          data {
-                            attributes {
-                              name
-                            }
+        tourismValue {
+          data {
+            id
+            attributes {
+              name
+              translation {
+                data {
+                  id
+                  attributes {
+                    code
+                    locale {
+                      value
+                      languages {
+                        data {
+                          attributes {
+                            name
                           }
                         }
                       }
@@ -121,23 +203,23 @@ export const places = gql`
               }
             }
           }
-          stateOfConservation {
-            data {
-              id
-              attributes {
-                name
-                translation {
-                  data {
-                    id
-                    attributes {
-                      code
-                      locale {
-                        value
-                        languages {
-                          data {
-                            attributes {
-                              name
-                            }
+        }
+        stateOfConservation {
+          data {
+            id
+            attributes {
+              name
+              translation {
+                data {
+                  id
+                  attributes {
+                    code
+                    locale {
+                      value
+                      languages {
+                        data {
+                          attributes {
+                            name
                           }
                         }
                       }
@@ -147,23 +229,23 @@ export const places = gql`
               }
             }
           }
-          recommendation {
-            data {
-              id
-              attributes {
-                name
-                translation {
-                  data {
-                    id
-                    attributes {
-                      code
-                      locale {
-                        value
-                        languages {
-                          data {
-                            attributes {
-                              name
-                            }
+        }
+        recommendation {
+          data {
+            id
+            attributes {
+              name
+              translation {
+                data {
+                  id
+                  attributes {
+                    code
+                    locale {
+                      value
+                      languages {
+                        data {
+                          attributes {
+                            name
                           }
                         }
                       }
@@ -173,23 +255,23 @@ export const places = gql`
               }
             }
           }
-          risk {
-            data {
-              id
-              attributes {
-                name
-                translation {
-                  data {
-                    id
-                    attributes {
-                      code
-                      locale {
-                        value
-                        languages {
-                          data {
-                            attributes {
-                              name
-                            }
+        }
+        risk {
+          data {
+            id
+            attributes {
+              name
+              translation {
+                data {
+                  id
+                  attributes {
+                    code
+                    locale {
+                      value
+                      languages {
+                        data {
+                          attributes {
+                            name
                           }
                         }
                       }
@@ -203,4 +285,5 @@ export const places = gql`
       }
     }
   }
+}
 `;
