dddc08fb1e799f8c0df9c0aef43b71370e8524b3
Diksha Deep <Diksha Deep@SPBLRLTU658.speridian.com>
2022-10-20 19:25:09 +0530

    add separate query

---
+++ src/query/events.ts
@@ -0,0 +1,197 @@
+import { gql } from "@apollo/client";
+
+export const events = gql`
+  query SearchTitle($search_one: String,$search_two: String, $search_three: String, $limit: Int, $skip: Int) {
+    visits(
+      pagination: {
+        limit: $limit
+        start: $skip
+      }
+      filters: {
+        or: [
+          # { siteType: { contains: $search_one } }
+          { siteDescription: { contains: $search_one } }
+          # { period: { contains: $search_one } }
+          { recordingTeam: { contains: $search_one } }
+          { fieldNarrative: { contains: $search_one } }
+          { keywords: { contains: $search_one } }
+          
+                    # { siteType: { contains: $search_two } }
+          { siteDescription: { contains: $search_two } }
+          # { period: { contains: $searcht_two } }
+          { recordingTeam: { contains: $search_two } }
+          { fieldNarrative: { contains: $search_two } }
+          { keywords: { contains: $search_one } }
+          
+                    # { siteType: { contains: $search_three } }
+          { siteDescription: { contains: $search_three } }
+          # { period: { contains: $search_three } }
+          { recordingTeam: { contains: $search_three } }
+          { fieldNarrative: { contains: $search_three } }
+          { keywords: { contains: $search_three } }
+        ]
+      }
+    ) {
+      meta {
+        pagination {
+          total
+          pageCount
+          pageSize
+          page
+        }
+      }
+      data {
+        id
+        attributes {
+          recordingTeam
+          siteDescription
+          updatedAt
+          keywords
+          visitNumber
+          latitude
+          longitude
+          uniqueId
+          type
+          period{
+            data{
+              id
+              attributes{
+                name
+              }
+            }
+          }
+          researchValue {
+            data {
+              id
+              attributes {
+                name
+                translation {
+                  data {
+                    id
+                    attributes {
+                      code
+                      locale {
+                        value
+                        languages {
+                          data {
+                            attributes {
+                              name
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          tourismValue {
+            data {
+              id
+              attributes {
+                name
+                translation {
+                  data {
+                    id
+                    attributes {
+                      code
+                      locale {
+                        value
+                        languages {
+                          data {
+                            attributes {
+                              name
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          stateOfConservation {
+            data {
+              id
+              attributes {
+                name
+                translation {
+                  data {
+                    id
+                    attributes {
+                      code
+                      locale {
+                        value
+                        languages {
+                          data {
+                            attributes {
+                              name
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          recommendation {
+            data {
+              id
+              attributes {
+                name
+                translation {
+                  data {
+                    id
+                    attributes {
+                      code
+                      locale {
+                        value
+                        languages {
+                          data {
+                            attributes {
+                              name
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+          risk {
+            data {
+              id
+              attributes {
+                name
+                translation {
+                  data {
+                    id
+                    attributes {
+                      code
+                      locale {
+                        value
+                        languages {
+                          data {
+                            attributes {
+                              name
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
