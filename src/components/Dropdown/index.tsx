5bdaa6bd3716fc5a0ef9b744236716526c5a3381
Siddhesh Sutar <siddhesh.sutar@speridian.com>
2022-10-21 21:08:22 +0530

    multi select css

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,11 +1,11 @@
 import Select from "@mui/material/Select";
 import Chip from '@mui/material/Chip';
 import { DropdownCompProps } from "../../types/DropdownComponent";
-import { MenuItem, Box, IconButton } from "@mui/material";
-import { ClearOutlined } from '@mui/icons-material';
+import { MenuItem, Box, Autocomplete, TextField } from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
 import { useState } from "react";
+import styles from './index.module.css';
 
 const DropdownComponent = ({
   className,
@@ -26,38 +26,83 @@ const DropdownComponent = ({
   return (
     <div className={className}>
       <FormControl sx={{ minWidth: 120, ...formControlSx }}>
-        {(focused || values) && (
-          <InputLabel id="demo-simple-select-helper-label">{label}</InputLabel>
-        )}
-        <Select
-          labelId={staticLabel}
-          id="simple-select"
-          multiple={multiple}
-          value={values}
-          name={name}
-          label={staticLabel}
-          placeholder={placeholder}
-          onChange={handleChange}
-          onFocus={(e) => setFocused(true)}
-          onBlur={(e) => setFocused(false)}
-          displayEmpty
-          sx={selectStylesSx}
-          renderValue={(selected: any) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected?.map((value: any) => (
-                <Chip key={value} label={value} />
-              ))}
+        {
+          multiple && <>
+            <Box className={`${styles['autocmplt-container']}`}>
+              <Autocomplete
+                multiple
+                id="tags-filled"
+                options={itemsList.map(item => item.label)}
+                size="small"
+                renderTags={(value: readonly string[], getTagProps) =>
+                  value.map((option: string, index: number) => (
+                    option && <Chip size = "small" variant="outlined" label={option} {...getTagProps({ index })} />
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="filled"
+                    label={staticLabel}
+                    placeholder={placeholder}
+                  />
+                )}
+                sx={{
+                  '& .MuiInputBase-root.MuiFilledInput-root:before': {
+                    borderBottom: 'none',
+                    display:' none'
+                  },
+                  '& .MuiFilledInput-root.MuiInputBase-sizeSmall': {
+                    paddingLeft: '0.5em',
+                    borderRadius: '0.3em'
+                  },
+                  '& .MuiFormControl-fullWidth.MuiTextField-root': {
+                    position: 'relative'
+                  },
+                  '& .MuiFormLabel-root.MuiInputLabel-root': {
+                    paddingLeft: '1em',
+                  }
+                }}
+              />
             </Box>
-          )}
-          endAdornment={<IconButton sx={{display: values.length>0 ? "visible": "none"}} onClick={handleClear} ><ClearOutlined fontSize={"small"} /></IconButton>}
-        >
-          <MenuItem disabled value="">
-            <em>{staticLabel}</em>
-          </MenuItem>
-          {itemsList.map((item) => (
-            <MenuItem key={item.label} value={item.value}>{item.label}</MenuItem>
-          ))}
-        </Select>
+          </>
+        }
+        {
+          !multiple && <>
+            {(focused || values) && (
+              <InputLabel id="demo-simple-select-helper-label">{label}</InputLabel>
+            )}
+            <Select
+              labelId={staticLabel}
+              id="simple-select"
+              multiple={multiple}
+              value={values}
+              name={name}
+              label={staticLabel}
+              placeholder={placeholder}
+              onChange={handleChange}
+              onFocus={(e) => setFocused(true)}
+              onBlur={(e) => setFocused(false)}
+              displayEmpty
+              sx={selectStylesSx}
+            // renderValue={(selected: any) => (
+            //   <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+            //     {selected?.map((value: any) => (
+            //       <Chip key={value} label={value} />
+            //     ))}
+            //   </Box>
+            // )}
+            // endAdornment={<IconButton sx={{display: values.length>0 ? "visible": "none"}} onClick={handleClear} ><ClearOutlined fontSize={"small"} /></IconButton>}
+            >
+              <MenuItem disabled value="">
+                <em>{staticLabel}</em>
+              </MenuItem>
+              {itemsList.map((item) => (
+                <MenuItem key={item.label} value={item.value}>{item.label}</MenuItem>
+              ))}
+            </Select>
+          </>
+        }
       </FormControl>
     </div>
   );
