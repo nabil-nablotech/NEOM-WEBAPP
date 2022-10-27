40c2a4f50c27bfb624353ddcc5677ddd539b87bf
Siddhesh Sutar <siddhesh.sutar@speridian.com>
2022-10-27 14:42:29 +0530

    Date err fixed

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,17 @@ import loginReducers  from './reducers/loginReducers';
 import searchResultsReducer from './reducers/searchResultsReducer';
 import refinedSearchReducer from './reducers/refinedSearchReducer';
 export const store = configureStore({
-    reducer: {
-      login: loginReducers,
-      searchResults: searchResultsReducer,
-      refinedSearch: refinedSearchReducer
-    },
-  });
+  reducer: {
+    login: loginReducers,
+    searchResults: searchResultsReducer,
+    refinedSearch: refinedSearchReducer
+  },
+  // Storing date in redux throwd cmd errors, hence to ignore those
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
   export type RootState = ReturnType<typeof store.getState>
   export type AppDispatch = typeof store.dispatch
