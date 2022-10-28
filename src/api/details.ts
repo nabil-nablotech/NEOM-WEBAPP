f02e6a3c023d942648423489b5b42296602067df
Diksha Deep <Diksha Deep@SPBLRLTU658.speridian.com>
2022-10-28 14:52:57 +0530

    add media

--- src/api/details.ts
+++ src/api/details.ts
@@ -1,12 +1,15 @@
 
 import {PlaceApi} from '../types/Place';
 import {EventApi} from '../types/Event';
+import {MediaApi} from '../types/Media';
 import client from "../utils/services/axiosClient";
 
 export const placeDetails = (payload: string): Promise<PlaceApi> =>
   client.get(`/api/custom/place/${payload}`).then((response) => response.data);
 
-  
 export const eventDetails = (payload: string): Promise<EventApi> =>
 client.get(`/api/custom/event/${payload}`).then((response) => response.data);
+
+export const mediaDetails = (payload: string): Promise<MediaApi> =>
+client.get(`/api/custom/media/${payload}`).then((response) => response.data);
 
