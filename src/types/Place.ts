6d21d78ef1520dee872467cadf9a0119ced1be66
Siddhesh Sutar <siddhesh.sutar@speridian.com>
2022-10-28 12:46:17 +0530

    places api intrgtn WIP

--- src/types/Place.ts
+++ src/types/Place.ts
@@ -1,4 +1,5 @@
 import { VisitAssociate } from "./Event";
+import { Media } from "./Media";
 
 export type PlaceApi = {
   id: string;
@@ -20,8 +21,10 @@ export type PlaceApi = {
   recommendation: string;
   risk: string;
   placeUIPath: string;
-  media_associates: MediaAssociates;
+  // media_associates: MediaAssociates;
+  media_associates: MediaAssociates2;
   visit_associates: VisitAssociate[]
+  libraryItems: Media[]
 };
 export type Place = {
   id: string;
@@ -29,6 +32,10 @@ export type Place = {
   longitude: number;
   attributes: PlaceApiResponse;
 };
+
+export type PlaceApiResponse2 = PlaceApiResponse & {
+  media_associates: MediaAssociates2;
+}
 
 export type PlaceApiResponse = {
   thumbnailUrl?: string;
@@ -71,6 +78,76 @@ export type MediaAssociates = {
     };
   }[];
 };
+
+export type ImageFormat = {
+  ext: string
+  hash: string
+  height: number
+  mime: string
+  name: string
+  path: string | null
+  size: number
+  url: string
+  width: number
+}
+
+export type MediaAssociates2_MediaObject_Format = {
+  large: ImageFormat
+  medium: ImageFormat
+  small: ImageFormat
+  thumbnail: ImageFormat
+}
+
+export type MediaAssociates2_MediaObject = {
+  alternativeText: string
+  caption: string
+  createdAt: string
+  ext: string
+  folderPath: string
+  formats: MediaAssociates2_MediaObject_Format
+  hash: string
+  height: number
+  id: number
+  mime: string
+  name: string
+  previewUrl: string | null
+  provider: string
+  provider_metadata: string | null
+  size: number
+  updatedAt: string
+  url: string
+  width: number
+}
+
+export type MediaAssociateObj = {
+  createdAt: string
+  id: number
+  media_unique_id: {
+    actionType: string
+    bearing: string | null
+    by: string | null
+    citation: string | null
+    createdAt: string
+    deleted: string | null
+    description: string
+    featuredImage: string | null
+    fileName: string
+    id: number
+    keywords: string | null
+    latitude: string | null
+    longitude: string | null
+    mediaUIPath: string | null
+    object: MediaAssociates2_MediaObject
+    objectURL: string | null
+    photographedBy: string | null
+    referenceURL: string | null
+    title: string
+    uniqueId: string
+    updatedAt: string
+  }
+}
+
+export type MediaAssociates2 = Array<MediaAssociateObj>
 
 export type Meta = {
   pagination: {
