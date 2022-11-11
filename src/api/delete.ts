eb49bd73d3b3cf05a008f731b6708ed0e8908133
Siddhesh Sutar <siddhesh.sutar@speridian.com>
2022-11-11 14:45:52 +0530

    url correction

--- src/api/delete.ts
+++ src/api/delete.ts
@@ -2,4 +2,4 @@ import { dbIdTypes, deleteRecordPayload } from "../types/Place";
 import client from "../utils/services/axiosClient";
 
 export const deleteRecord = (dbId: dbIdTypes, id: number): Promise<any> =>
-    client.put(`/api/custom/delete/${dbId === 'library' ? 'media' : '' }/${id}`).then((response) => response.data);
+    client.put(`/api/custom/delete/${dbId === 'library' ? 'media' : dbId }/${id}`).then((response) => response.data);
