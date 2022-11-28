0eea0e9c56a4b35f273f797ddc980eef7e252708
azam.chaudhary <azam.chaudhary@speridian.com>
2022-11-28 11:24:55 -0500

    timeout changes done

--- src/components/SessionTimeOut/index.tsx
+++ src/components/SessionTimeOut/index.tsx
@@ -1,37 +1,27 @@
-
 import { useState, useEffect } from 'react';
 import { addEventListeners,  removeEventListeners } from './sessionTimeOutUtil'
 import useLogout from "../../hooks/useLogout";
 
 const SessionTimeOut = () => {
     const { clientLogout } = useLogout();
-    const [isWarningModalOpen, setWarningModalOpen] = useState(false);
+    const [timeOutStatus, setTimeOutStatus] = useState(false);
   useEffect(() => {
-    const createTimeout1 = () => setTimeout(()=>{ 
-      setWarningModalOpen(true);
-    },5000)
-
-    const createTimeout2 = () => setTimeout(() => {
+    let timeout = setTimeout(() => {
         clientLogout();
     },600000)
 
     const listener = () => {
-      if(!isWarningModalOpen){
-        clearTimeout(timeout)
-        timeout = createTimeout1();
-      }
+      setTimeOutStatus(!timeOutStatus);
+      clearTimeout(timeout);
     } 
-
     // Initialization
-    let timeout = isWarningModalOpen  ? createTimeout2() : createTimeout1()
     addEventListeners(listener);
-
     // Cleanup
     return () => {
       removeEventListeners(listener);
       clearTimeout(timeout);
     }
-  },[isWarningModalOpen])
+  },[timeOutStatus])
   return (
     <>
     </>
@@ -39,3 +29,4 @@ const SessionTimeOut = () => {
 };
 
 export default SessionTimeOut;
+
