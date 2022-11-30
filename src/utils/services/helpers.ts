41eeb1ce5955accf6474d6901df0e4dcb14ff39a
Siddhesh Sutar <siddhesh.sutar@speridian.com>
2022-11-30 17:42:00 +0530

    intials undefined issue fix

--- src/utils/services/helpers.ts
+++ src/utils/services/helpers.ts
@@ -113,7 +113,7 @@ export const validatePassword = (s: string) => {
 };
 
 export function stringAvatar(name: string) {
-  return `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`;
+  return `${name.split(" ")[0][0] ?? ''}${name.split(" ")[1][0] ?? ''}`;
 }
 
 export function passwordGenerator() {
